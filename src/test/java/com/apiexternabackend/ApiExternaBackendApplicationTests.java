d04f49e7959e6b8c399d252b0fe9072d69a002e5
Lucas Mendes <lucassabadinimendes@gmail.com>
2026-04-28 16:50:15 -0300

    First Commit

---
+++ src/test/java/com/apiexternabackend/ApiExternaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiexternabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExternaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
